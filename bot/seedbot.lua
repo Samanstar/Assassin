ea61baa55cb2c763ca6be785b1952f5909982c2b
Samanstar <samanbeeza.net81@gmail.com>
2016-03-15 17:09:56 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {179709966},--Sudo users
+    sudo_users = {196740258},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
